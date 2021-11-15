01ceca87d4cd7bb8650b7536f5e7b855e2b2325f
Dixie Cravens <dcravens@gmail.com>
2021-11-15 14:03:12 -0500

    changed Edit onClick button response, set up fragment Manager

--- app/src/main/java/com/example/moneycache/EditBudgetActivity.java
+++ app/src/main/java/com/example/moneycache/EditBudgetActivity.java
@@ -1,10 +1,13 @@
 package com.example.moneycache;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.Toast;
 
 public class EditBudgetActivity extends AppCompatActivity {
 
@@ -14,7 +17,9 @@ public class EditBudgetActivity extends AppCompatActivity {
     private EditText d;
     private EditText dr;
     private EditText s;
-    private String category;
+    private String placeholder;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,9 @@ public class EditBudgetActivity extends AppCompatActivity {
 
         //get Budget Items from controller to fill in
         //https://stackoverflow.com/questions/33164886/android-textview-do-not-concatenate-text-displayed-with-settext
+        controller = new EditBudgetController(this);
         controller.start();
+
         i = findViewById(R.id.income_category);
         i.setText(getString(R.string.income_text, controller.getIncome()));
         b = findViewById(R.id.bills_category);
@@ -35,74 +42,65 @@ public class EditBudgetActivity extends AppCompatActivity {
         s = findViewById(R.id.savings_category);
         s.setText(getString(R.string.savings_text, controller.getSavings()));
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-    //need to add a EditBudgetFragment with an onClick from any 'Edit' button.
+        //need to add a EditBudgetFragment with an onClick from any 'Edit' button.
         //see https://developer.android.com/guide/fragments/create#add-programmatic
-//       if (savedInstanceState != null) {
-//            // bring in data with Bundle for each unique edit view
-//            // each edit button is going to have to have a unique identifier.
-//            Bundle bundle = new Bundle();
-//            //data to pass to fragment-- this needs to know which category is being edited and which frag_placeholder to place it in
-//            //how do I pass info with an OnClick event?
-//            //https://stackoverflow.com/questions/37105066/android-data-binding-pass-arguments-to-onclick-method
-//            bundle.putInt("some_int", 0);
-//            getSupportFragmentManager().beginTransaction()
-//                    .setReorderingAllowed(true)
-//                    .add(R.id.frag_placeholder_income, EditBudgetFragment.class, bundle)
-//                    .commit();
-        //}
+        if (savedInstanceState != null) {
+            // data to pass to fragment-- this needs to know what the editText should contain
+            Bundle bundle = new Bundle();
+            bundle.putString("amount", String.valueOf(i.getText()));
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.setReorderingAllowed(true);
+            if (placeholder != null){
+                switch (placeholder){
+                    case ("income"):
+                        transaction.add(R.id.frag_placeholder_income,EditBudgetFragment.class, bundle);
+                        break;
+                    case ("bills"):
+                        transaction.add(R.id.frag_placeholder_bills,EditBudgetFragment.class, bundle);
+                        break;
+                    case ("discretionary"):
+                        transaction.add(R.id.frag_placeholder_discretionary,EditBudgetFragment.class, bundle);
+                        break;
+                    case ("debt_reduction"):
+                        transaction.add(R.id.frag_placeholder_debt_reduction,EditBudgetFragment.class, bundle);
+                        break;
+                    case ("savings"):
+                        transaction.add(R.id.frag_placeholder_savings,EditBudgetFragment.class, bundle);
+                        break;
+                    default:
+                        Toast.makeText(this, "No category found.", Toast.LENGTH_LONG).show();
 
-    }
-    public void onEditClick(String category) {
-        this.category = category;
-        String placeholder;
-        switch (category){
-            case "Income":
-                placeholder = "frag_placeholder_income";
-                break;
-            case "Bill":
-                placeholder = "";
-                break;
-            case "Discretionary":
-                placeholder = "frag_placeholder_discretionary";
-                break;
-            case "Debt_reduction":
-                placeholder = "frag_placeholder_debt_reduction";
-                break;
-            case "Savings":
-                placeholder = "frag_placeholder_savings";
-                break;
-
+                }
+            }
+            transaction.commit();
         }
+
     }
 
     /**
      * receives onClick from (category specific))_edit_button
-     * passes category to onEditClick()
+     * fills category and placeholder id for fragment build
      */
-    private void onEditIncome() {
-        String cat = "Income";
-        onEditClick(cat);
+    public void handleEditButton(View view) {
+        if (view.getId() == R.id.income_edit_button) {
+            placeholder = "income";
+        } else if (view.getId() == R.id.bill_edit_button) {
+            placeholder = "bills";
+        } else if (view.getId() == R.id.discretionary_edit_button) {
+            placeholder = "discretionary";
+        } else if (view.getId() == R.id.debt_reduction_edit_button) {
+            placeholder = "debt_reduction";
+        } else if (view.getId() == R.id.savings_edit_button) {
+            placeholder = "savings";
+        } else {
+            placeholder = null;
+        }
     }
-    private void onEditBills() {
-        String cat = "Bills";
-        onEditClick(cat);
+    public void handleUpdateButton(View view) {
+        //hide the fragment: https://developer.android.com/guide/fragments/transactions#views
     }
-    private void onEditDiscretionary() {
-        String cat = "Discretionary";
-        onEditClick(cat);
-    }
-    private void onEditDebtReduction() {
-        String cat = "Debt_reduction";
-        onEditClick(cat);
-    }
-    private void onEditSavings() {
-        String cat = "Savings";
-        onEditClick(cat);;
-    }
+
 }
+//how do I pass info with an OnClick event?
+//https://stackoverflow.com/questions/37105066/android-data-binding-pass-arguments-to-onclick-method
