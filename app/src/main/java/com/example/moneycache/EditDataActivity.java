ff8cd336e2732226d2e0cc92abff15cfb73f1d14
Dixie Cravens <dcravens@gmail.com>
2021-11-18 11:02:31 -0500

    Spinner working, EditData now main

--- app/src/main/java/com/example/moneycache/EditDataActivity.java
+++ app/src/main/java/com/example/moneycache/EditDataActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class EditDataActivity extends AppCompatActivity {
@@ -24,5 +25,14 @@ public class EditDataActivity extends AppCompatActivity {
         dataController.start();
 
         //recyclerView = findViewById(R.id.recyclerView);
+
+        Spinner spinner = (Spinner) findViewById(R.id.assign_category_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
