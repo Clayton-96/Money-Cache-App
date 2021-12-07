37efe2a01ac6f0243c260165eac58cc42088206f
Dixie Cravens <dcravens@gmail.com>
2021-12-07 18:11:32 -0500

    Profile page working

--- app/src/main/java/com/example/moneycache/ProfileController.java
+++ app/src/main/java/com/example/moneycache/ProfileController.java
@@ -1,4 +1,16 @@
 package com.example.moneycache;
 
 public class ProfileController {
+    ProfileActivity profActivity;
+    DataModel model;
+
+    public ProfileController (ProfileActivity profActivity) {
+        this.profActivity = profActivity;
+        model = new DataModel();
+    }
+
+
+
+
 }
+
