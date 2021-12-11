3e4cef1d09a105c0a6f59d246a4ed9a2269a41f3
Dixie Cravens <dcravens@gmail.com>
2021-12-11 01:02:24 -0500

    code clean up and comments

--- app/src/main/java/com/example/moneycache/DashboardActivity.java
+++ app/src/main/java/com/example/moneycache/DashboardActivity.java
@@ -19,14 +19,8 @@ import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    //NavigationActivity navigation;
     DashboardController controller;
-    List<String> pieChartData;
     TextView b, d, dr, s;
-
-    //Variables needed for pie chart
-    Float bAmt, drAmt, dAmt, sAmt;
-    Float income; //(income = controller.getIncome())
 
 
     @Override
@@ -35,43 +29,41 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         setTitle("Dashboard");
     // when we create the page we run a start method that creates all the data needed for the page
-    //
         controller = new DashboardController(this);
         controller.start();
+        //pieChartData = controller.getItems();
 
-
-        pieChartData = controller.getItems();
-        Log.d("Created controller", "onCreate: ");
         b = (TextView) findViewById(R.id.bills_alert);
         d = (TextView) findViewById(R.id.discretionary_alert);
         dr = (TextView) findViewById(R.id.debtreduction_alert);
         s = (TextView) findViewById(R.id.savings_alert);
-        bAmt = controller.billsAmt;
-        dAmt = controller.discretionaryAmt;
-        drAmt = controller.debtReductionAmt;
-        sAmt = controller.savingsAmt;
+        Float bAmt = controller.billsAmt;
+        Float dAmt = controller.discretionaryAmt;
+        Float drAmt = controller.debtReductionAmt;
+        Float sAmt = controller.savingsAmt;
 
-
-
-
+        //alert boxes
         if (controller.billsAmtGreen) {
             b.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
         } else {
             b.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
         }
         b.setText(getString(R.string.bills_alert, String.format("%.2f", bAmt)));
+
         if (controller.discretionaryAmtGreen) {
             d.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
         } else {
             d.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
         }
         d.setText(getString(R.string.discretionary_alert, String.format("%.2f", dAmt)));
+
         if (controller.debtReductionAmtGreen) {
             dr.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
         } else {
             dr.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
         }
         dr.setText(getString(R.string.debtreduction_alert, String.format("%.2f", drAmt)));
+
         if (controller.savingsAmtGreen) {
             s.setBackgroundColor(ContextCompat.getColor(this, R.color.green));
         } else {
@@ -79,31 +71,22 @@ public class DashboardActivity extends AppCompatActivity {
         }
         s.setText(getString(R.string.savings_alert, String.format("%.2f", sAmt)));
 
-//        //Pie Chart Update
-//            // Calculate the slice size and update the pie chart:
-//            ProgressBar pieChart = findViewById(R.id.stats_progressbar);
-//
-//            Float totalSpent = controller.getTotalSpent();
-//            int bills = (int)controller.pieChartBills();
-//            int discretionary = (int)controller.pieChartDiscretionary();
-//            int debtReduction = (int)controller.pieChartReduction();
-//            int savings = (int)controller.pieChartSavings();
-//            //int progress = (int) (d * 100);
-//            pieChart.setProgress(bills);
-//            pieChart.setProgress(discretionary);
-//            pieChart.setProgress(debtReduction);
-//            pieChart.setProgress(savings);
+        //create pie chart
         drawPie();
-
 
     }
 
+    /**
+     * Draws an animated pie chart based on total spent/each category spending
+     * https://github.com/razerdp/AnimatedPieView
+     */
     public void drawPie() {
         int bills = (int)controller.pieChartBills();
         int discretionary = (int)controller.pieChartDiscretionary();
         int debtReduction = (int)controller.pieChartReduction();
         int savings = (int)controller.pieChartSavings();
 
+        //this code came from AnimatedPieView
         AnimatedPieView mAnimatedPieView = findViewById(R.id.animatedPieView);
         AnimatedPieViewConfig config = new AnimatedPieViewConfig();
         config.startAngle(-90)// Starting angle offset
@@ -114,26 +97,9 @@ public class DashboardActivity extends AppCompatActivity {
 
       .duration(2000).textSize(30);// draw pie animation duration
 
-// The following two sentences can be replace directly 'mAnimatedPieView.start (config); '
         mAnimatedPieView.applyConfig(config);
         mAnimatedPieView.start();
     }
-    //
-//    private void updateChart(){
-//        // Update the text in a center of the chart:
-//        TextView numberOfCals = findViewById(R.id.number_of_calories);
-//        numberOfCals.setText(String.valueOf(calsBurned) + " / " + calsConsumed);
-//
-//        // Calculate the slice size and update the pie chart:
-//        ProgressBar pieChart = findViewById(R.id.stats_progressbar);
-//        double d = (double) calsBurned / (double) calsConsumed;
-//        int progress = (int) (d * 100);
-//        pieChart.setProgress(progress);
-//    }
-
-
-
-
 
 
 //****** menu for navigation *************
